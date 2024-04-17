18c8115dc4d9066e20f724246527e5c5b2017325
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-04-17 17:33:51 +0200

    update

---
+++ WebAppBacknd/Data/DataExtansions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAppBacknd.Data;
+
+public static class DataExtansions
+{
+    public static void MigrateDb(this WebApplication app)
+    {
+        using var scope = app.Services.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
+        dbContext.Database.Migrate();
+    }
+}
