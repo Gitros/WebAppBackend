cfed587261077b8fa32b03f08f35bc45410e0763
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-04-17 13:40:42 +0200

    updt

---
+++ WebAppBacknd/Dtos/CreateGameDto.cs
@@ -0,0 +1,8 @@
+﻿namespace WebAppBacknd.Dtos;
+
+public record class CreateGameDto(
+    string Name,
+    string Genre,
+    decimal Price,
+    DateOnly ReleaseDate
+);
--- WebAppBacknd/Program.cs
+++ WebAppBacknd/Program.cs
@@ -1,6 +1,34 @@
+using WebAppBacknd.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<GameDto> games = [
+    new (
+        1,
+        "Cyberpunk 2077",
+        "Fighting",
+        19.99M,
+        new DateOnly(2019, 7, 15)),
+    new (
+        2,
+        "The Binding of Isaac",
+        "Roguelike",
+        10.99M,
+        new DateOnly(2015, 5, 21)),
+    new(
+        3,
+        "League of Legends",
+        "Battle arena",
+        105.20M,
+        new DateOnly(2009, 2, 12)),
+];
+
+// GET /games
+app.MapGet("games", () => games);
+
+// GET /games/1
+app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id));
 
 app.Run();
