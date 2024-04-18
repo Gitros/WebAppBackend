e02fae8c2737518d5f92f7c9a44de491eecb433b
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-04-18 13:06:09 +0200

    mapping entities

--- WebAppBacknd/Endpoints/GamesEndpoints.cs
+++ WebAppBacknd/Endpoints/GamesEndpoints.cs
@@ -1,6 +1,7 @@
 ﻿using WebAppBacknd.Data;
 using WebAppBacknd.Dtos;
 using WebAppBacknd.Entities;
+using WebAppBacknd.Mapping;
 
 namespace WebAppBacknd.Endpoints;
 
@@ -48,27 +49,13 @@ public static class GamesEndpoints
         // POST /games
         group.MapPost("/", (CreateGameDto newGame, GameStoreContext dbContext) =>
         {
-            Game game = new()
-            {
-                Name = newGame.Name,
-                Genre = dbContext.Genres.Find(newGame.GenreId),
-                GenreId = newGame.GenreId,
-                Price = newGame.Price,
-                ReleaseDate = newGame.ReleaseDate
-            };
+            Game game = newGame.ToEntity();
+            game.Genre = dbContext.Genres.Find(newGame.GenreId);
 
             dbContext.Games.Add(game);
             dbContext.SaveChanges();
 
-            GameDto gameDto = new(
-                game.Id,
-                game.Name,
-                game.Genre!.Name,
-                game.Price,
-                game.ReleaseDate
-            );
-
-            return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, gameDto);
+            return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game.ToDto());
         })
         .WithParameterValidation();
 
